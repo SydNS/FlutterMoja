5c09f522701b63f2edbf52a8d5172fa04eb3358b
SydNS <40373744+SydNS@users.noreply.github.com>
2021-05-08 13:05:06 -0700

    changed the icons for the app and alseo added an image

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
           MaterialApp(
+            debugShowCheckedModeBanner: false,
             home: Scaffold(
               backgroundColor: Colors.black12,
-              appBar:AppBar(title: Text("DEans first fluttter"),
+              appBar:AppBar(title: Text("Deans first fluttter"),
                 backgroundColor: Colors.blueGrey,
-
               ),
+              body:Center(
+
+              ) ,
             ),
           ),
       );
