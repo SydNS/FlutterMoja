d44239db6cf8d0cf9253dcfa511c3fb1bf317f10
SydNS <40373744+SydNS@users.noreply.github.com>
2021-05-08 10:51:59 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+          MaterialApp(
+            home: Scaffold(
+              backgroundColor: Colors.black12,
+              appBar:AppBar(title: Text("DEans first fluttter"),
+                backgroundColor: Colors.blueGrey,
+
+              ),
+            ),
+          ),
+      );
+      }
+
