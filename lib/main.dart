c53312e8bafa2efc84583284344049eaed4e570e
SydNS <40373744+SydNS@users.noreply.github.com>
2021-05-09 03:09:42 -0700

    moving from the function apps to widets

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-          MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(
-              backgroundColor: Colors.black12,
-              appBar:AppBar(title: Text("Deans first fluttter"),
-                backgroundColor: Colors.blueGrey,
-              ),
-              body:Center(
-                child: Image.asset('images/quotes.jpg'),
-
-
-              ) ,
-            ),
-          ),
-      );
+  runApp(MyApp());
       }
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black12,
+        appBar:AppBar(title: Text("Deans first fluttter"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body:Center(
+          child: Image.asset('images/quotes.jpg',),
+
+        ) ,
+      ),
+    );
+  }
+}
 
