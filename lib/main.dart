ec58c197c4ae32afe2582d7b8ca90430b5f2fedf
SydNS <40373744+SydNS@users.noreply.github.com>
2021-05-09 03:49:09 -0700

    using lists of objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kasanje/Transactions.dart';
 
 void main() {
   runApp(MyHome());
@@ -21,21 +22,26 @@ class MyHome extends StatelessWidget {
 
 class MyApp extends StatelessWidget{
 
+  final List<Transactions> transactions=[
+    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Scaffold(
         backgroundColor: Colors.black12,
         appBar:AppBar(title: Text("Deans first flutter"),
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Colors.black,
         ),
         body:Column(
           children: [
-            Card(child:Image.asset('images/quotes.jpg'),elevation: 10,   ),
-            Card(child:Image.asset('images/quotes.jpg'),elevation: 10,   ),
-            Card(child:Image.asset('images/quotes.jpg'),elevation: 10,   ),
-            Card(child:Image.asset('images/quotes.jpg'),elevation: 10,   ),
-            Card(child:Image.asset('images/quotes.jpg'),elevation: 10,   )
+            Card(
+              child: Image.asset('images/quotes.jpg'), elevation: 10 ),
 
           ],
         ) ,
