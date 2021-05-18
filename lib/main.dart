5e4906f66060d033e4ca0a396759e0450a37fe9e
SydNS <40373744+SydNS@users.noreply.github.com>
2021-05-18 05:50:04 -0700

    trying out a button

--- lib/main.dart
+++ lib/main.dart
@@ -5,68 +5,103 @@ import 'package:kasanje/Transactions.dart';
 void main() {
   runApp(MyHome());
       }
-
 class MyHome extends StatelessWidget {
-  const MyHome({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        title: "DEan",
-      home: MyApp(),
-
+      home: LoginDemo(),
     );
   }
 }
 
-class MyApp extends StatelessWidget{
+class LoginDemo extends StatefulWidget {
+  @override
+  _LoginDemoState createState() => _LoginDemoState();
+}
 
-  final List<Transactions> transactions=[
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-    Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
-  ];
-
+class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        backgroundColor: Colors.black12,
-        appBar:AppBar(title: Text("Deans first flutter"),
-          backgroundColor: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample App'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.only(left: 20.0, right: 30.0),
+          ),
+          onPressed: () {},
+          child: Text('Hello'),
         ),
-        body:Column(
-            children: <Widget>[
-              Container(child:  Card(
-                  child: Image.asset('images/quotes.jpg'), elevation: 10 ),
-              ),
-              Column(
-                children: transactions.map((e){
-                    return Card(
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              child: Text(e.title),
-                              width: 50,
-                            ),
-                            Column(
-                              children: [
-                                Text("WEWERWE")
-                              ],
-                            )
-                          ],
-                        ), elevation: 10, );
-                }).toList(),)
-          ],
-        ) ,
-
+      ),
     );
   }
-}
 
+
+}
+// class MyHome extends StatelessWidget {
+//   const MyHome({Key key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//         title: "DEan",
+//       home: MyApp(),
+//
+//     );
+//   }
+// }
+//
+// class MyApp extends StatelessWidget{
+//
+//   final List<Transactions> transactions=[
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//     Transactions(1, '12/2/2020', 20000, 'bootsandsneakers'),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//         backgroundColor: Colors.black12,
+//         appBar:AppBar(title: Text("Deans first flutter"),
+//           backgroundColor: Colors.black,
+//         ),
+//         body:Column(
+//             children: <Widget>[
+//               Container(child:  Card(
+//                   child: Image.asset('images/quotes.jpg'), elevation: 10 ),
+//               ),
+//               Column(
+//                 children: transactions.map((e){
+//                     return Card(
+//                         child: Row(
+//                           children: <Widget>[
+//                             Container(
+//                               child: Text(e.title),
+//                               width: 50,
+//                             ),
+//                             Column(
+//                               children: [
+//                                 Text("WEWERWE"),
+//                               ],
+//                             )
+//                           ],
+//                         ), elevation: 10, );
+//                 }).toList(),)
+//           ],
+//         ) ,
+//
+//     );
+//   }
+// }
+//
