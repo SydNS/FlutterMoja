c514ac80030e6e8e2d5a8087d9289df37ede962f
SydNS <40373744+SydNS@users.noreply.github.com>
2021-05-20 03:28:57 -0700

    applying a bottom navigation in the screen

--- lib/SecndPage.dart
+++ lib/SecndPage.dart
@@ -1,13 +1,47 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SecndPage extends StatelessWidget {
+class SecndPage extends StatefulWidget {
+
+  SecndPage({Key key}) : super(key : key);
+  @override
+  _MyApp createState() => _MyApp();
+}
+class _MyApp extends State {
+
+
+  var _currentPage = 0;
+
+  var _pages = [
+    Text("Page 1 - Announcements"),
+    Text("Page 2 - Birthdays"),
+    Text("Page 3 - Data")
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:  Text("Hey"),),
+      appBar: AppBar(
+        title: Text("Hey"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.announcement),
+              // ignore: deprecated_member_use
+              title: Text("Announcements")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.cake),
+              // ignore: deprecated_member_use
+              title: Text("Birthdays")),
+          BottomNavigationBarItem(icon: Icon(Icons.cloud), title: Text("Data")),
+        ],
+          currentIndex : _currentPage,
+          fixedColor : Colors.red,
+          onTap : (int inIndex) {
+            setState(() { _currentPage = inIndex; });
+          }
+      ),
       body: Column(
         children: [
           Container(
@@ -19,10 +53,8 @@ class SecndPage extends StatelessWidget {
               Text("get"),
             ],
           )
-
         ],
       ),
     );
   }
 }
-
